37c864d476c0d339c9ef8157fcf2fa951d024f70
DevHamaad <hamaadmajeed786@gmail.com>
2025-06-20 22:15:10 +0500

    created the login_screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          color: Colors.white,
+          padding: EdgeInsetsGeometry.symmetric(vertical: 30,horizontal: 10),
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                child: Image.asset("assets/images/login.jpg"),
+                ),
+              ),
+              Expanded(
+                child: Container(
+
+                ),
+              ),
+              Expanded(
+                child: Container(
+
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
