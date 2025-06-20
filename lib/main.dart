bdfac0d7002ef150ef5a765b4641cdde49266706
DevHamaad <hamaadmajeed786@gmail.com>
2025-06-20 23:01:33 +0500

    added snackbar to show the email entered by the user

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LoginScreen();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -15,112 +15,133 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Image.asset(
-                    "assets/images/login.jpg",
-                    height: MediaQuery.of(context).size.height * 0.3,
-                  ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Image.asset(
+                  "assets/images/login.jpg",
+                  height: MediaQuery.of(context).size.height * 0.3,
                 ),
-                const SizedBox(height: 20),
-                // Form Section
-                Form(
-                  key: formKey,
-                  child: Column(
-                    children: [
-                      const Text(
-                        "Sign In",
-                        style: TextStyle(fontSize: 25),
+              ),
+              const SizedBox(height: 20),
+              // Form Section
+              Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    const Text(
+                      "Sign In",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                    const SizedBox(height: 15),
+                    TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        hintText: "xyz@gmail.com",
+                        labelText: "Enter Your Email",
+                        prefixIcon: const Icon(Icons.email),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
                       ),
-                      const SizedBox(height: 15),
-                      TextFormField(
-                        controller: emailController,
-                        decoration: InputDecoration(
-                          hintText: "xyz@gmail.com",
-                          labelText: "Enter Your Email",
-                          prefixIcon: const Icon(Icons.email),
-                          border: OutlineInputBorder(
+                      validator: (value){
+                        if(value==null||value.isEmpty){
+                          return "Please Enter Your Email";
+                        }
+                        if(!value.contains("@")||!value.contains(".com")){
+                          return "Please Enter Valid EmailAddress";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 15),
+                    TextFormField(
+                      obscureText: _obsecurePassword,
+                      obscuringCharacter: "*",
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        labelText: "Enter Your Password",
+                        prefixIcon: const Icon(Icons.lock),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _obsecurePassword = !_obsecurePassword;
+                            });
+                          },
+                          icon: Icon(
+                            _obsecurePassword
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
+                        ),
+                      ),
+                      validator: (value){
+                        if(value==null||value.isEmpty){
+                          return "Please Enter Your Password";
+                        }
+                        if(value.length<8){
+                          return "Password Must Be At Least 8 Characters";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(10),
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 15),
-                      TextFormField(
-                        obscureText: _obsecurePassword,
-                        obscuringCharacter: "*",
-                        controller: passwordController,
-                        decoration: InputDecoration(
-                          hintText: "Password",
-                          labelText: "Enter Your Password",
-                          prefixIcon: const Icon(Icons.lock),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          suffixIcon: IconButton(
-                            onPressed: () {
-                              setState(() {
-                                _obsecurePassword = !_obsecurePassword;
-                              });
-                            },
-                            icon: Icon(
-                              _obsecurePassword
-                                  ? Icons.visibility_off
-                                  : Icons.visibility,
-                            ),
-                          ),
+                        onPressed: () {
+                          if(formKey.currentState!.validate()){
+                            final email = emailController.text;
+                            final password = passwordController.text;
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('Login Success: $email')),
+                            );
+                          }
+                        },
+                        child: const Padding(
+                          padding: EdgeInsets.all(12.0),
+                          child: Text("Login", style: TextStyle(fontSize: 18)),
                         ),
                       ),
-                      const SizedBox(height: 20),
-                      SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.black,
-                            foregroundColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                          onPressed: () {
-                            // TODO: Validate and login
-                          },
-                          child: const Padding(
-                            padding: EdgeInsets.all(12.0),
-                            child: Text("Login", style: TextStyle(fontSize: 18)),
-                          ),
-                        ),
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: const [
+                        Text("Forgot Password?"),
+                      ],
+                    ),
+                    const SizedBox(height: 40),
+                    SizedBox(
+                      width: double.infinity,
+                      child: OutlinedButton(
+                        onPressed: () {
+
+                        },
+                        child: const Text("Don't Have an Account? Register Here"),
                       ),
-                      const SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: const [
-                          Text("Forgot Password?"),
-                        ],
-                      ),
-                      const SizedBox(height: 40),
-                      SizedBox(
-                        width: double.infinity,
-                        child: OutlinedButton(
-                          onPressed: () {
-                            // TODO: Navigate to Register
-                          },
-                          child: const Text("Don't Have an Account? Register Here."),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
